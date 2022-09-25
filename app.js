6ad145a036ca495a303761ba180915e52b04ba0a
NivasDonthireddy <dlnivas@hotmail.com>
2022-09-25 16:15:43 +0530

    pug finished

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const  bodyParser = require('body-parser');
 
-const adminRouter = require('./routes/admin');
+const {adminRouter} = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine','pug');
+app.set('views','views');
+
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,23 @@
-const { Router } = require("express");
 const path = require('path');
 
-const router = Router();
+const express = require('express');
 
-router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../views/add-product.html')); 
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
